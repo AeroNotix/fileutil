db8bb1a3be2a90db1dcbe3f85cb9ac1d3933e18e
Aaron France <aaron.l.france@gmail.com>
2013-09-04 17:46:04 +0200

    Added plenty of comments and TODOs.

--- fileutil.go
+++ fileutil.go
@@ -8,6 +8,13 @@ import (
 	"path/filepath"
 )
 
+// CopyDirectory copies all files and folders underneath src/ and
+// copies them under the dst/ directory, recursively.
+//
+// TODO: The error handling in this function is a bit wonky. We log
+// errors and continue on others. I need to do a couple of tests to
+// see if legitimate cases need to `continue' after hitting one of
+// these errors.
 func CopyDirectory(dst, src string) error {
 	fi, err := os.Stat(src)
 	if err != nil {
@@ -25,6 +32,9 @@ func CopyDirectory(dst, src string) error {
 	}
 
 	for _, origfile := range subfiles {
+		// TODO: It probably doesn't need the EvalSymlinks
+		// here since we check to see if the filepath is a
+		// symlink after and do something different if it is.
 		source_file, err := filepath.EvalSymlinks(filepath.Join(src, origfile.Name()))
 		if err != nil {
 			return err
@@ -35,6 +45,9 @@ func CopyDirectory(dst, src string) error {
 			return err
 		}
 		destination_file := filepath.Join(dst, file.Name())
+		// If the original file is a symlink then we can
+		// simply create a new symlink with the appropriate
+		// link name.
 		if issym, err := IsSymLink(filepath.Join(src, origfile.Name())); issym && err == nil {
 			linkname, err := os.Readlink(filepath.Join(src, origfile.Name()))
 			if err != nil {
@@ -46,6 +59,8 @@ func CopyDirectory(dst, src string) error {
 			}
 			continue
 		}
+		// If it's a directory we're looking at, recurse to
+		// copy its contents.
 		if file.IsDir() {
 			err = CopyDirectory(destination_file, source_file)
 			if err != nil {
@@ -53,6 +68,7 @@ func CopyDirectory(dst, src string) error {
 				continue
 			}
 		} else {
+			// Otherwise we just copy the original file.
 			err = CopyFile(destination_file, source_file)
 			if err != nil {
 				log.Println(err)
@@ -63,6 +79,7 @@ func CopyDirectory(dst, src string) error {
 	return nil
 }
 
+// CopyFile takes two pathnames and copies src into dst.
 func CopyFile(dst, src string) error {
 	source_file, err := os.Open(src)
 	if err != nil {
@@ -82,10 +99,22 @@ func CopyFile(dst, src string) error {
 	return err
 }
 
+// Detects whether the path is a symbolic link or not.
 func IsSymLink(fpath string) (bool, error) {
 	fi, err := os.Lstat(fpath)
 	if err != nil {
 		return false, err
 	}
 	return fi.Mode()&os.ModeSymlink == os.ModeSymlink, nil
+}
+
+// MakeAbs detects if the path is an absolute path and if it is not an
+// absolute path then it joins together the currect working directory
+// path along with the original relative pathname.
+func MakeAbs(fpath string) string {
+	if !filepath.IsAbs(fpath) {
+		cwd, _ := os.Getwd()
+		return filepath.Join(cwd, fpath)
+	}
+	return fpath
 }
